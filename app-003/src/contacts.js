3ee9a8145991f51fc27656108ab07fe014a92ae7
Marcio Bueno <marciobueno@gmail.com>
2024-05-27 20:30:51 -0300

    refatorar código contacts.js criando constantes e reusando-as no código

--- app-003/src/contacts.js
+++ app-003/src/contacts.js
@@ -1,25 +1,30 @@
+const baseURL = "https://parseapi.back4app.com/classes/Contact";
+
+const headers = {
+  "X-Parse-Application-Id": "IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR",
+  "X-Parse-REST-API-Key": "e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q",
+  "Content-Type": "application/x-www-form-urlencoded",
+};
+
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/x-www-form-urlencoded",
+};
+
 export async function getContacts(query) {
-  const response = await fetch('https://parseapi.back4app.com/classes/Contact', {
+  const response = await fetch(baseURL, {
     method: "GET",
-    headers: {
-        'X-Parse-Application-Id': 'IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR',
-        'X-Parse-REST-API-Key': 'e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
+    headers: headersJson,
   });
   const data = await response.json();
   return data.results;
 }
 
 export async function createContact() {
-  const response = await fetch("https://parseapi.back4app.com/classes/Contact", {
+  const response = await fetch(baseURL, {
     method: "POST",
-	  headers: {
-		  "X-Parse-Application-Id": "IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR",
-		  "X-Parse-REST-API-Key": "e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q",
-		  "Content-Type": "application/json",
-	  },
-	  body: JSON.stringify({}),
+    headers: headersJson,
+    body: JSON.stringify({}),
   });
   const contact = await response.json();
   return contact;
@@ -27,47 +32,37 @@ export async function createContact() {
 
 export async function getContact(id) {
   const whereClause = JSON.stringify({ objectId: id });
-  const url = encodeURI(`https://parseapi.back4app.com/classes/Contact/?where=${whereClause}`);
+  const url = encodeURI(`${baseURL}/?where=${whereClause}`);
   const response = await fetch(url, {
     method: "GET",
-    headers: {
-      'X-Parse-Application-Id': 'IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR',
-      'X-Parse-REST-API-Key': 'e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q'
-    }
+    headers: headers,
   });
   const data = await response.json();
   const contact = data.results[0];
-  console.log('contact', contact);
+  console.log("contact", contact);
   return contact ?? null;
 }
 
 export async function updateContact(id, updates) {
-  console.log('updates', updates);
+  console.log("updates", updates);
   updates.favorite = updates.favorite === "true";
 
-  const response = await fetch(`https://parseapi.back4app.com/classes/Contact/${id}`, {
-    method: 'PUT',
-    headers: {
-      'X-Parse-Application-Id': 'IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR',
-      'X-Parse-REST-API-Key': 'e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(updates)
+  const response = await fetch(`${baseURL}/${id}`, {
+    method: "PUT",
+    headers: headersJson,
+    body: JSON.stringify(updates),
   });
 
   let contact = await response.json();
-  contact = { ...contact, ...updates};
-  console.log('backend return + merge updates: ', contact);
+  contact = { ...contact, ...updates };
+  console.log("backend return + merge updates: ", contact);
   return contact;
 }
 
 export async function deleteContact(id) {
-  const response = await fetch(`https://parseapi.back4app.com/classes/Contact/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'X-Parse-Application-Id': 'IWUd0IFatPNUQq9Mb1qnFxVFzrqWATAvQl7OPbnR',
-      'X-Parse-REST-API-Key': 'e13dZgND1g2bizlNv8yJBPEuxZDY9Bgfw4X7cJ7q',
-    },
+  const response = await fetch(`${baseURL}/${id}`, {
+    method: "DELETE",
+    headers: headers,
   });
   return response.ok;
 }
